4db67e81c8269c452f46ddc70d2921f7ad689f12
vilesport <89498002+vilesport@users.noreply.github.com>
2024-04-09 20:48:46 +0700

    Update source code

--- boot.asm
+++ boot.asm
@@ -1,6 +1,6 @@
-jmp main
+jmp Main
 
-clrscr:
+Clrscr:
     mov ah, 0x07
     mov al, 0
     mov cx, 0
@@ -14,76 +14,76 @@ clrscr:
     int 0x10
     ret
 
-main:
-    call clrscr
+Main:
+    call Clrscr
     lea di, [menu + 0x7c00]
-    call print
+    call Print
     lea di, [buffer + 0x7c00]
-    call read
+    call Read
     xor ax, ax
     mov [di + 101], ax
     lea di, [cal + 0x7c00]
-    call print
+    call Print
     lea di, [buffer + 0x7c00]
-    call print
+    call Print
     lea di, [cal + 0x7c00 + 28]
-    call print
-    call end_line
+    call Print
+    call End_line
     lea di, [cal + 0x7c00 + 39]
-    call print
-    call read_char
-    jmp main
+    call Print
+    call Read_char
+    jmp Main
 
-put_char:
+Put_char:
     mov ah, 0x0e
     mov al, bl
     int 0x10
     ret 
 
-print:
+Print:
     mov bl, [di]
     cmp bl, 0
-    je end_func
-    call put_char
+    je End_func
+    call Put_char
     add di, 1
-    jmp print
+    jmp Print
 
-read_char:
+Read_char:
     xor ax, ax
     int 0x16
     ret
 
-read:
-    call read_char
+Read:
+    call Read_char
     mov bl, al
     cmp bl, 0x0d
-    je end_line
+    je End_line
     cmp bl, 0x20
-    jl read
+    jl Read
     cmp bl, 0x7e
-    jg read
-    call put_char
+    jg Read
+    call Put_char
     lea cx, [buffer + 0x7c00 + 100]
     cmp di, cx
-    jge read
-    call write_char
+    jge Read
+    call Write_char
     add di, 1
-    jmp read
+    jmp Read
 
-end_line:
-    mov bl, 0
-    call write_char
-    mov bl, 13
-    call put_char
-    mov bl, 10
-    call put_char
-    ret
-
-end_func:
-    ret
-
-write_char:
+Write_char:
     mov [di], bl
+    ret
+
+End_line:
+    mov bl, 0
+    call Write_char
+    mov bl, 13
+    call Put_char
+    mov bl, 10
+    call Put_char
+    ret
+
+End_func:
     ret
 
 menu:
